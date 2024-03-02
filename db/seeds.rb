bf50e9d59aa58f5d0891d4e8ccfac985695893bf
Joankrugg <joan_krugg@live.fr>
2024-03-02 12:20:24 +0100

    add model Color

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Yeast.create({name: 'Lager'})
-Yeast.create({name: 'Ale'})
-Yeast.create({name: 'Spontanée'})
-Yeast.create({name: 'Hybride'})
+#Yeast.create({name: 'Lager'})
+#Yeast.create({name: 'Ale'})
+#Yeast.create({name: 'Spontanée'})
+#Yeast.create({name: 'Hybride'})
+
+Color.create({name: 'Blanche'})
+Color.create({name: 'Blonde'})
+Color.create({name: 'Ambrée'})
+Color.create({name: 'Ruby'})
+Color.create({name: 'Brune'})
+Color.create({name: 'Noire'})
