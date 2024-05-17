6e97f96817e2cf1c56a4852a666376f0607af319
Joankrugg <joan_krugg@live.fr>
2024-05-17 17:16:23 +0200

    places ok on beer show

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :beer
   geocoded_by :city
+  validates :name, presence: true
   after_validation :geocode, if: ->(obj){ obj.city.present? and obj.city_changed? }
 end
