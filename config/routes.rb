5ca6b2b5cd3f80bc5c4b41997965869252b174fb
Joankrugg <joan_krugg@live.fr>
2024-06-12 14:31:09 +0200

    add 2 legal pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'politics', to: 'pages#politics'
+  get 'datas', to: 'pages#datas'
   resources :beers do
     resources :places
   end
