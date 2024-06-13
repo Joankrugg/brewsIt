cf9693e82513565ee36f4a55595dd345994d2857
Joankrugg <joan_krugg@live.fr>
2024-06-13 16:45:11 +0200

    bretagne seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require "csv"
 require "open-uri"
 
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'bordeaux.csv'), headers: true) do |row|
+CSV.foreach(Rails.root.join('db', 'seeds', 'bretagne.csv'), headers: true) do |row|
   begin
     spot = Spot.new
     spot.name = row['name']
