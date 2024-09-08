4cd8e167db36406fee4967189a28d8baa19de023
Joankrugg <joan_krugg@live.fr>
2024-09-08 21:02:30 +0200

    max_level_greater_than_min_level

--- app/models/trackr.rb
+++ app/models/trackr.rb
@@ -9,4 +9,15 @@ class Trackr < ApplicationRecord
   has_many :flavours, through: :trackr_flavours
   validates :level_min, :level_max, presence: true
 
+  # Validation personnalisée pour s'assurer que level_max > level_min
+  validate :max_level_greater_than_min_level
+
+  private
+
+  def max_level_greater_than_min_level
+    if level_max.present? && level_min.present? && level_max <= level_min
+      errors.add(:level_max, "doit être supérieur à level_min")
+    end
+  end
+
 end
